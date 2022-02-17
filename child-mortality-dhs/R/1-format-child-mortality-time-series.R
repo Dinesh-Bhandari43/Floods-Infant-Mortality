91439b4b3d9b5facecc99edeae86fcb5c551fb4d
FrancoisRerolle <rerollefrancois@gmail.com>
2022-02-17 12:37:06 +0100

    Initiated 1-dm folder

---
+++ child-mortality-dhs/R/1-format-child-mortality-time-series.R
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# @Organization - UCSF
+# @Project - Effects of floods on child mortality in Bangladesh
+# @Author - Francois Rerolle, rerollefrancois@gmail.com
+# @Description - This file formats the child mortality time series 
+#               out of the DHS data.
+#-------------------------------------------------------------------------------
+
+# Source configuration code
+library(here)
+source(here("child-mortality-dhs/R", "0-config.R"))
