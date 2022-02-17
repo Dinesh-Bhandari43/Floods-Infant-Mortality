96164d183e0f7bcd1e2b6585516059b962443fd1
FrancoisRerolle <rerollefrancois@gmail.com>
2022-02-17 10:48:50 +0100

    Creation of folders within repository

---
+++ child-mortality-dhs/R/0-config.R
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# @Organization - UCSF
+# @Project - Effects of floods on child mortality in Bangladesh
+# @Author - Francois Rerolle, rerollefrancois@gmail.com
+# @Description - This file is responsible for configuring the projects,
+#               declaring global variables, path to data, loading packages, etc.
+#-------------------------------------------------------------------------------
+
+# Load packages
+library(here)
+library(tidyverse)
