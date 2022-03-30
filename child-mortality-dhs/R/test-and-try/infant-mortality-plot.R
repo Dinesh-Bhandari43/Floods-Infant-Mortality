e817ef001e16b6d39c0b922cb2324bec2ed28772
FrancoisRerolle <rerollefrancois@gmail.com>
2022-03-30 11:41:40 +0200

    started modeling

--- child-mortality-dhs/R/test-and-try/infant-mortality-plot.R
+++ child-mortality-dhs/R/test-and-try/infant-mortality-plot.R
@@ -11,7 +11,9 @@ monthly_time_series <- readRDS(file = here("data/final", "monthly_time_series"))
 ## Aggregate flooded vs non flooded
 monthly_time_series_qs_flooded_vs_non_flooded <- (monthly_time_series
                                                   %>% group_by(Birth_Date_Month_CMC, Flooded)
-                                                  %>% summarise(Number_Of_Birth_QS = sum(Number_Of_Birth_QS, na.rm = T),
+                                                  %>% summarise(Number_Of_Birth = sum(Number_Of_Birth, na.rm = T),
+                                                                Number_Of_Dead_Birth = sum(Number_Of_Dead_Birth, na.rm = T),
+                                                                Number_Of_Birth_QS = sum(Number_Of_Birth_QS, na.rm = T),
                                                                 Number_Of_Dead_Birth_QS = sum(Number_Of_Dead_Birth_QS, na.rm = T))
                                                   %>% mutate(Infant_Mortality_Quarterly_Smoothed = Number_Of_Dead_Birth_QS/Number_Of_Birth_QS,
                                                              Infant_Mortality = Infant_Mortality_Quarterly_Smoothed)
@@ -23,10 +25,9 @@ monthly_time_series_qs_flooded_vs_non_flooded <- (monthly_time_series
                                                              Season_Change = replace(Season_Change, is.na(Season_Change), FALSE),
                                                              Grouping_Season = cumsum(Season_Change))
                                                   %>% group_by(Grouping_Season, Flooded)
-                                                  %>% mutate(Number_Of_Birth_QS_Season = sum(Number_Of_Birth_QS, na.rm = T),
-                                                             Number_Of_Dead_Birth_QS_Season = sum(Number_Of_Dead_Birth_QS, na.rm = T),
-                                                             Infant_Mortality_Quarterly_Smoothed_Season = Number_Of_Dead_Birth_QS_Season/Number_Of_Birth_QS_Season,
-                                                             Infant_Mortality_Season = Infant_Mortality_Quarterly_Smoothed_Season)
+                                                  %>% mutate(Number_Of_Birth_Season = sum(Number_Of_Birth, na.rm = T),
+                                                             Number_Of_Dead_Birth_Season = sum(Number_Of_Dead_Birth, na.rm = T),
+                                                             Infant_Mortality_Season = Number_Of_Dead_Birth_Season/Number_Of_Birth_Season)
                                                   )
 
 monthly_birth_plot <- (monthly_time_series_qs_flooded_vs_non_flooded
@@ -35,12 +36,12 @@ monthly_birth_plot <- (monthly_time_series_qs_flooded_vs_non_flooded
                        + geom_point()
                        # + geom_smooth(method = "lm", se = F)
                        # + geom_line()
-                       + geom_smooth(se = T, span = 0.1)
+                       + geom_smooth(se = T, span = 0.08)
                        + scale_x_continuous(breaks = seq(1063, 1411, by = 12), labels = paste("July", 1988:2017))
                        # + xlim(c(1135, 1291))
                        + ylim(c(0,15))
-                       # + geom_vline(xintercept = 1291) # July 2007
-                       # + geom_vline(xintercept = 1255) # July 2004
+                       + geom_vline(xintercept = 1291) # July 2007
+                       + geom_vline(xintercept = 1255) # July 2004
                        + xlab("Date")
                        + ylab("Infant mortality (%)")
                        + theme(axis.text.x = element_text(angle = 30))
@@ -61,8 +62,8 @@ monthly_time_series_qs_flooded_vs_non_flooded_RR <- (monthly_time_series_qs_floo
                                                                 Infant_Mortality_RR_ub = exp(log(Infant_Mortality_RR) + 1.96 * Infant_Mortality_SE_ln_RR)
                                                                 )
                                                      %>% mutate(Infant_Mortality_Season_RR = Infant_Mortality_Season.x / Infant_Mortality_Season.y,
-                                                                Infant_Mortality_Season_SE_ln_RR = sqrt((1/Number_Of_Dead_Birth_QS_Season.x) - (1/Number_Of_Birth_QS_Season.x)
-                                                                                                 + (1/Number_Of_Dead_Birth_QS_Season.y) - (1/Number_Of_Birth_QS_Season.y)),
+                                                                Infant_Mortality_Season_SE_ln_RR = sqrt((1/Number_Of_Dead_Birth_Season.x) - (1/Number_Of_Birth_Season.x)
+                                                                                                 + (1/Number_Of_Dead_Birth_Season.y) - (1/Number_Of_Birth_Season.y)),
                                                                 Infant_Mortality_Season_RR_lb = exp(log(Infant_Mortality_Season_RR) - 1.96 * Infant_Mortality_Season_SE_ln_RR),
                                                                 Infant_Mortality_Season_RR_ub = exp(log(Infant_Mortality_Season_RR) + 1.96 * Infant_Mortality_Season_SE_ln_RR)
                                                      )
@@ -75,14 +76,14 @@ monthly_birth_plot_RR <- (monthly_time_series_qs_flooded_vs_non_flooded_RR
                        %>% ggplot(aes(x = Birth_Date_Month_CMC, y = Infant_Mortality_RR, group = 1, col = Season.x))
                        + geom_point()
                        # + geom_smooth(method = "lm", se = F)
-                       # + geom_smooth(se = T, span = 0.08)
-                       + geom_errorbar(aes(ymin = Infant_Mortality_RR_lb, ymax = Infant_Mortality_RR_ub))
+                       + geom_smooth(se = T, span = 0.08)
+                       # + geom_errorbar(aes(ymin = Infant_Mortality_RR_lb, ymax = Infant_Mortality_RR_ub))
                        + scale_x_continuous(breaks = seq(1063, 1411, by = 12), labels = paste("July", 1988:2017))
                        # + scale_x_continuous(breaks = seq(1063, 1411-6*12, by = 12), labels = paste("July", 1988:(2017-6)))
                        # + xlim(c(1063, 1411 - 6*12))
-                       + ylim(c(0,7.5))
-                       + geom_vline(xintercept = 1291, col = "orange") # July 2007
-                       + geom_vline(xintercept = 1255, col = "orange") # July 2004
+                       # + ylim(c(0,7.5))
+                       # + geom_vline(xintercept = 1291, col = "orange") # July 2007
+                       # + geom_vline(xintercept = 1255, col = "orange") # July 2004
                        + geom_hline(yintercept = 1) # RR = 1
                        + xlab("Date")
                        # + facet_wrap(~Season.x)
