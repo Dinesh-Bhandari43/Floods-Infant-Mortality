50bcc829a1b2d1248b97e55ba955fc0efc95017b
FrancoisRerolle <rerollefrancois@gmail.com>
2023-05-30 16:10:49 -0400

    Added via the Open Science Framework

---
+++ child-mortality-dhs/R/test-and-try/5c-infant-mortality-model.R
@@ -0,0 +1,306 @@
+#-------------------------------------------------------------------------------
+
+# @Organization - UCSF
+# @Project - Effects of floods on child mortality in Bangladesh
+# @Author - Francois Rerolle, rerollefrancois@gmail.com
+# @Description - This code runs the models to estimate effects (RR) of
+# living in flood prone area on infant mortality over time and per season
+
+#-------------------------------------------------------------------------------
+
+# Source configuration code
+library(here)
+source(here("child-mortality-dhs/R", "0-config.R"))
+
+#-------------------------------------------------------------------------------
+
+## Load data
+monthly_time_series_0_quartile <- readRDS(file = here("data/final", "monthly_time_series_0_quartile"))
+monthly_time_series_1_quartile <- readRDS(file = here("data/final", "monthly_time_series_1_quartile"))
+monthly_time_series_2_quartile <- readRDS(file = here("data/final", "monthly_time_series_2_quartile"))
+monthly_time_series_3_quartile <- readRDS(file = here("data/final", "monthly_time_series_3_quartile"))
+BGD_chirps <- readRDS(file = here("data/final", "BGD_chirps"))
+
+monthly_time_series_list <- list(monthly_time_series_0_quartile,
+                                 monthly_time_series_1_quartile,
+                                 monthly_time_series_2_quartile,
+                                 monthly_time_series_3_quartile)
+
+## Merge to precipitation data
+monthly_time_series_chirps_list <-
+  lapply(X = monthly_time_series_list,
+         FUN = function(x){
+           (x %>%
+              mutate(Year = 1900 + floor((Birth_Date_Month_CMC-1)/12),
+                     Month = Birth_Date_Month_CMC - 12*(Year - 1900),
+                     Season = factor(ifelse(Month %in% c(5:10), "Rainy", "Dry")),
+                     Half = factor(floor((Year - 1988)/15)),
+                     Decade = factor(floor((Year - 1988)/10)),
+                     Quinquenat = factor(floor((Year - 1988)/5)))
+            %>% group_by(Flooded)
+            %>% mutate(New_Season = Season != lag(Season),
+                       New_Season = replace(New_Season, is.na(New_Season), FALSE),
+                       Grouping_Season = cumsum(New_Season))
+            %>% ungroup()
+            %>% left_join(BGD_chirps)
+            %>% filter(Year!=1987)
+            %>% filter(Birth_Date_Month_CMC != 1413)
+            )
+           }
+         )
+
+## Model
+model_gam <- gam(Number_Of_Dead_Birth ~ Flooded*relevel(Season, ref = "Rainy")*relevel(Decade, ref = "0") +
+                   s(Birth_Date_Month_CMC) +
+                   s(Mean_Precipitation_mm) +
+                   offset(log(Number_Of_Birth)),
+                 data = monthly_time_series_chirps_list[[1]],
+                 select = T,
+                 family = "nb")
+
+summary(model_gam)
+plot(model_gam)
+# 
+# ## Extract effect estimates Halves
+# RR_flooded_vs_non_flooded_tb_quartile <- tibble(RR = rep(NA, 4),
+#                                                 RR_SE = rep(NA, 4),
+#                                                 Season = rep(c("Dry", "Rainy"), 2),
+#                                                 Half = rep(c("0","1"), each = 2))
+# 
+# RR_flooded_vs_non_flooded_tb <- rbind(RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "0 quartile"),
+#                                       RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "1 quartile"),
+#                                       RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "2 quartile"),
+#                                       RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "3 quartile"))
+# 
+# for (j in 1:4){
+#   for (i in 1:4){
+#     model_gam <- gam(Number_Of_Dead_Birth ~ Flooded*relevel(Season, ref = RR_flooded_vs_non_flooded_tb$Season[i])*relevel(Half, ref = RR_flooded_vs_non_flooded_tb$Half[i]) + s(Birth_Date_Month_CMC) + s(Mean_Precipitation_mm) + offset(log(Number_Of_Birth)),
+#                      data = monthly_time_series_chirps_list[[j]],
+#                      family = "nb")
+#     
+#     RR_flooded_vs_non_flooded_tb$RR[i + (j-1)*4] <- summary(model_gam)$p.coeff[2]
+#     RR_flooded_vs_non_flooded_tb$RR_SE[i + (j-1)*4] <- summary(model_gam)$se[2]
+#   }
+# }
+# 
+# RR_plot <- (RR_flooded_vs_non_flooded_tb %>%
+#               # filter(Exposure != "3 quartile") %>%
+#               # filter(Exposure != "1 quartile") %>%
+#               mutate(Half = replace(Half, Half == "0", "1988-2002"),
+#                      Half = replace(Half, Half == "1", "2003-2017")) %>%
+#               ggplot(aes(x = Season, y = exp(RR), col = Season)) +
+#               geom_point(size = 4) +
+#               geom_errorbar(aes(ymin = exp(RR - 1.96*RR_SE),
+#                                 ymax = exp(RR + 1.96*RR_SE)),
+#                             size = 2) +
+#               geom_hline(aes(yintercept = 1)) +
+#               facet_wrap(Exposure~Half, ncol = 2) +
+#               ylab("Risk ratio for living in\nflood-prone areas vs non-flood prone areas") +
+#               coord_flip()
+#             )
+# 
+# RR_plot
+
+## Extract effect estimates Decade
+RR_flooded_vs_non_flooded_tb_quartile <- tibble(RR = rep(NA, 6),
+                                                RR_SE = rep(NA, 6),
+                                                Season = rep(c("Dry", "Rainy"), 3),
+                                                Decade = rep(c("0","1","2"), each = 2))
+
+RR_flooded_vs_non_flooded_tb <- rbind(RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "0 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "1 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "2 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "3 quartile"))
+
+for (j in 1:4){
+  for (i in 1:6){
+    model_gam <- gam(Number_Of_Dead_Birth ~ Flooded*relevel(Season, ref = RR_flooded_vs_non_flooded_tb$Season[i])*relevel(Decade, ref = RR_flooded_vs_non_flooded_tb$Decade[i]) + s(Birth_Date_Month_CMC) + s(Mean_Precipitation_mm) + offset(log(Number_Of_Birth)),
+                     data = monthly_time_series_chirps_list[[j]],
+                     family = "nb")
+    
+    RR_flooded_vs_non_flooded_tb$RR[i + (j-1)*6] <- summary(model_gam)$p.coeff[2]
+    RR_flooded_vs_non_flooded_tb$RR_SE[i + (j-1)*6] <- summary(model_gam)$se[2]
+  }
+}
+
+RR_plot <- (RR_flooded_vs_non_flooded_tb %>%
+              filter(Exposure != "3 quartile") %>%
+              # filter(Exposure != "1 quartile") %>%
+              mutate(Decade = replace(Decade, Decade == "0", "1988-1997"),
+                     Decade = replace(Decade, Decade == "1", "1998-2007"),
+                     Decade = replace(Decade, Decade == "2", "2008-2017")) %>%
+              ggplot(aes(x = Season, y = exp(RR), col = Season)) +
+              geom_point(size = 4) +
+              geom_errorbar(aes(ymin = exp(RR - 1.96*RR_SE),
+                                ymax = exp(RR + 1.96*RR_SE)),
+                            size = 2) +
+              geom_hline(aes(yintercept = 1)) +
+              facet_wrap(Exposure~Decade, ncol = 3) +
+              ylab("Risk ratio for living in\nflood-prone areas vs non-flood prone areas") +
+              coord_flip()
+)
+
+RR_plot
+
+
+# ## Extract effect estimates Quinquenat
+# RR_flooded_vs_non_flooded_tb_quartile <- tibble(RR = rep(NA, 12),
+#                                                 RR_SE = rep(NA, 12),
+#                                                 Season = rep(c("Dry", "Rainy"), 6),
+#                                                 Quinquenat = rep(c("0","1", "2", "3", "4", "5"), each = 2))
+# 
+# RR_flooded_vs_non_flooded_tb <- rbind(RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "0 quartile"),
+#                                       RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "1 quartile"),
+#                                       RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "2 quartile"),
+#                                       RR_flooded_vs_non_flooded_tb_quartile %>%
+#                                         mutate(Exposure = "3 quartile"))
+# 
+# for (j in 1:4){
+#   for (i in 1:12){
+#     model_gam <- gam(Number_Of_Dead_Birth ~ Flooded*relevel(Season, ref = RR_flooded_vs_non_flooded_tb$Season[i])*relevel(Quinquenat, ref = RR_flooded_vs_non_flooded_tb$Quinquenat[i]) + s(Birth_Date_Month_CMC) + s(Mean_Precipitation_mm) + offset(log(Number_Of_Birth)),
+#                      data = monthly_time_series_chirps_list[[j]],
+#                      family = "nb")
+#     
+#     RR_flooded_vs_non_flooded_tb$RR[i + (j-1)*12] <- summary(model_gam)$p.coeff[2]
+#     RR_flooded_vs_non_flooded_tb$RR_SE[i + (j-1)*12] <- summary(model_gam)$se[2]
+#   }
+# }
+# 
+# RR_plot <- (RR_flooded_vs_non_flooded_tb %>%
+#               # filter(Exposure != "3 quartile") %>%
+#               # filter(Exposure != "1 quartile") %>%
+#               mutate(Quinquenat = replace(Quinquenat, Quinquenat == "0", "1988-1992"),
+#                      Quinquenat = replace(Quinquenat, Quinquenat == "1", "1993-1997"),
+#                      Quinquenat = replace(Quinquenat, Quinquenat == "2", "1998-2002"),
+#                      Quinquenat = replace(Quinquenat, Quinquenat == "3", "2003-2007"),
+#                      Quinquenat = replace(Quinquenat, Quinquenat == "4", "2008-2012"),
+#                      Quinquenat = replace(Quinquenat, Quinquenat == "5", "2013-2017")) %>%
+#               ggplot(aes(x = Season, y = exp(RR), col = Season)) +
+#               geom_point(size = 4) +
+#               geom_errorbar(aes(ymin = exp(RR - 1.96*RR_SE),
+#                                 ymax = exp(RR + 1.96*RR_SE)),
+#                             size = 2) +
+#               geom_hline(aes(yintercept = 1)) +
+#               facet_wrap(Exposure~Quinquenat, ncol = 6) +
+#               ylab("Risk ratio for living in\nflood-prone areas vs non-flood prone areas") +
+#               coord_flip()
+# )
+# 
+# RR_plot
+
+
+## Extract effect estimates Year
+RR_flooded_vs_non_flooded_tb_quartile <- tibble(RR = rep(NA, 60),
+                                                RR_SE = rep(NA, 60),
+                                                Season = rep(c("Dry", "Rainy"), 30),
+                                                Year = rep(1:30, each = 2))
+
+RR_flooded_vs_non_flooded_tb <- rbind(RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "0 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "1 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "2 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "3 quartile"))
+
+for (j in 1:4){
+  for (i in 1:60){
+    model_gam <- gam(Number_Of_Dead_Birth ~ Flooded*relevel(Season, ref = RR_flooded_vs_non_flooded_tb$Season[i])*relevel(factor(Year), ref = RR_flooded_vs_non_flooded_tb$Year[i]) + s(Birth_Date_Month_CMC) +
+                       s(Mean_Precipitation_mm) +
+                       offset(log(Number_Of_Birth)),
+                     data = monthly_time_series_chirps_list[[j]],
+                     family = "nb")
+    
+    RR_flooded_vs_non_flooded_tb$RR[i + (j-1)*60] <- summary(model_gam)$p.coeff[2]
+    RR_flooded_vs_non_flooded_tb$RR_SE[i + (j-1)*60] <- summary(model_gam)$se[2]
+  }
+}
+
+RR_plot <- (RR_flooded_vs_non_flooded_tb %>%
+              filter(Exposure == "0 quartile") %>%
+              # filter(Exposure != "1 quartile") %>%
+              # mutate(Decade = replace(Decade, Decade == "0", "1988-1997"),
+              #        Decade = replace(Decade, Decade == "1", "1998-2007"),
+              #        Decade = replace(Decade, Decade == "2", "2008-2017")) %>%
+              ggplot(aes(x = Season, y = exp(RR), col = Season)) +
+              geom_point(size = 4) +
+              geom_errorbar(aes(ymin = exp(RR - 1.96*RR_SE),
+                                ymax = exp(RR + 1.96*RR_SE)),
+                            size = 2) +
+              ylim(0,5) +
+              geom_hline(aes(yintercept = 1)) +
+              facet_wrap(~Year, ncol = 5) +
+              ylab("Risk ratio for living in\nflood-prone areas vs non-flood prone areas") +
+              coord_flip()
+)
+
+RR_plot
+
+
+## Extract effect estimates Grouping Season
+RR_flooded_vs_non_flooded_tb_quartile <- tibble(RR = rep(NA, 60),
+                                                RR_SE = rep(NA, 60),
+                                                Season = rep(c("Dry", "Rainy"), 30),
+                                                Grouping_Season = rep(1:60))
+
+RR_flooded_vs_non_flooded_tb <- rbind(RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "0 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "1 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "2 quartile"),
+                                      RR_flooded_vs_non_flooded_tb_quartile %>%
+                                        mutate(Exposure = "3 quartile"))
+
+for (j in 1:4){
+  for (i in 1:60){
+    model_gam <- gam(Number_Of_Dead_Birth ~ Flooded*relevel(factor(Grouping_Season), ref = RR_flooded_vs_non_flooded_tb$Grouping_Season[i]) +
+                       s(Birth_Date_Month_CMC) +
+                       offset(log(Number_Of_Birth)),
+                     data = monthly_time_series_chirps_list[[j]],
+                     family = "nb")
+    
+    RR_flooded_vs_non_flooded_tb$RR[i + (j-1)*60] <- summary(model_gam)$p.coeff[2]
+    RR_flooded_vs_non_flooded_tb$RR_SE[i + (j-1)*60] <- summary(model_gam)$se[2]
+  }
+}
+
+RR_plot <- (RR_flooded_vs_non_flooded_tb %>%
+              filter(Exposure == "0 quartile") %>%
+              # filter(Exposure != "1 quartile") %>%
+              # mutate(Decade = replace(Decade, Decade == "0", "1988-1997"),
+              #        Decade = replace(Decade, Decade == "1", "1998-2007"),
+              #        Decade = replace(Decade, Decade == "2", "2008-2017")) %>%
+              ggplot(aes(x = Season, y = exp(RR), col = Season)) +
+              geom_point(size = 4) +
+              geom_errorbar(aes(ymin = exp(RR - 1.96*RR_SE),
+                                ymax = exp(RR + 1.96*RR_SE)),
+                            size = 2) +
+              ylim(0,5) +
+              geom_hline(aes(yintercept = 1)) +
+              facet_wrap(~Grouping_Season, ncol = 5) +
+              ylab("Risk ratio for living in\nflood-prone areas vs non-flood prone areas") +
+              coord_flip()
+)
+
+RR_plot
+
+#-------------------------------------------------------------------------------
+
+# Save dataset
+saveRDS(RR_flooded_vs_non_flooded_tb,
+        file = here("data/final", "RR_flooded_vs_non_flooded_tb"))
+
+#-------------------------------------------------------------------------------
