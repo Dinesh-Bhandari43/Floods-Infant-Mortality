9de2e7ba38b077222fe5d8f1543c2124e2177c9f
Ben Arnold <bfarnold@gmail.com>
2023-11-03 15:00:45 -0700

    Update infant mortality follow-up period to include all deaths through 11m, corresponding to the infant mortality rate

--- child-mortality-dhs/R/1-dm/8-finalize-birth-records.R
+++ child-mortality-dhs/R/1-dm/8-finalize-birth-records.R
@@ -28,98 +28,167 @@ matched_mothers_6_frequency <- readRDS(file = here("data/final", "matched_mother
 matched_mothers_7_frequency <- readRDS(file = here("data/final", "matched_mothers_7_frequency"))
 matched_mothers_flood_year <- readRDS(file = here("data/final", "matched_mothers_flood_year"))
 
+#-------------------------------------
+# summarize the overall number of
+# mothers and births in the dataset
+#-------------------------------------
+
+# number of births
+cat("\n The number of unique births in the dataset is:",paste(nrow(birth_records_flooded)))
+
+# number of mothers
+cat("\n The number of unique mothers in the dataset is:",paste(length(unique(birth_records_flooded$caseid))))
+
+#-------------------------------------
 ## Filter to matched mothers only
-birth_records_flooded_0_quartile <- (birth_records_flooded %>%
-                                       right_join(matched_mothers_0_quartile %>%
-                                                    dplyr::select(caseid, weights, subclass)) %>%
-                                       mutate(Flooded = Flooded_0_quartile,
-                                              Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-                                     )
+#-------------------------------------
 
-birth_records_flooded_1_quartile <- (birth_records_flooded %>%
-                                       right_join(matched_mothers_1_quartile %>%
-                                                    dplyr::select(caseid, weights, subclass)) %>%
-                                       mutate(Flooded = Flooded_1_quartile,
-                                              Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-                                     )
+# primary exposure, any flooding
+birth_records_flooded_0_quartile <- matched_mothers_0_quartile %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_0_quartile,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
-birth_records_flooded_2_quartile <- (birth_records_flooded %>%
-                                       right_join(matched_mothers_2_quartile %>%
-                                                    dplyr::select(caseid, weights, subclass)) %>%
-                                       mutate(Flooded = Flooded_2_quartile,
-                                              Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-                                     )
+# first quartile (25th percentile of flooded days)
+birth_records_flooded_1_quartile <- matched_mothers_1_quartile %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_1_quartile,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
-birth_records_flooded_3_quartile <- (birth_records_flooded %>%
-                                       right_join(matched_mothers_3_quartile %>%
-                                                    dplyr::select(caseid, weights, subclass)) %>%
-                                       mutate(Flooded = Flooded_3_quartile,
-                                              Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-                                     )
+# second quartile (50th percentile of flooded days)
+birth_records_flooded_2_quartile <-  matched_mothers_2_quartile %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_2_quartile,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
+  
+# third quartile (75th percentile of flooded days)
+birth_records_flooded_3_quartile <- matched_mothers_3_quartile %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_3_quartile,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
+# non-flood prone areas
 birth_records_flooded_control <- (birth_records_flooded %>%
                                     filter(Flooded_0_quartile == FALSE) %>%
                                     mutate(Flooded = "Control",
-                                           Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
+                                           Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months)))
+                                  )
+
+# located flooded 1 to 7 times
+birth_records_flooded_1_frequency <- matched_mothers_1_frequency %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_1_Frequency,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
+  
+
+birth_records_flooded_2_frequency <- matched_mothers_2_frequency %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_2_Frequency,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
+  
+
+birth_records_flooded_3_frequency <- matched_mothers_3_frequency %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_3_Frequency,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
 
-birth_records_flooded_1_frequency <- (birth_records_flooded %>%
-                                       right_join(matched_mothers_1_frequency %>%
-                                                    dplyr::select(caseid, weights, subclass)) %>%
-                                       mutate(Flooded = Flooded_1_Frequency,
-                                              Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
+birth_records_flooded_4_frequency <- matched_mothers_4_frequency %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_4_Frequency,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
-birth_records_flooded_2_frequency <- (birth_records_flooded %>%
-                                        right_join(matched_mothers_2_frequency %>%
-                                                     dplyr::select(caseid, weights, subclass)) %>%
-                                        mutate(Flooded = Flooded_2_Frequency,
-                                               Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
 
-birth_records_flooded_3_frequency <- (birth_records_flooded %>%
-                                        right_join(matched_mothers_3_frequency %>%
-                                                     dplyr::select(caseid, weights, subclass)) %>%
-                                        mutate(Flooded = Flooded_3_Frequency,
-                                               Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
+birth_records_flooded_5_frequency <- matched_mothers_5_frequency %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_5_Frequency,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
-birth_records_flooded_4_frequency <- (birth_records_flooded %>%
-                                        right_join(matched_mothers_4_frequency %>%
-                                                     dplyr::select(caseid, weights, subclass)) %>%
-                                        mutate(Flooded = Flooded_4_Frequency,
-                                               Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
 
-birth_records_flooded_5_frequency <- (birth_records_flooded %>%
-                                        right_join(matched_mothers_5_frequency %>%
-                                                     dplyr::select(caseid, weights, subclass)) %>%
-                                        mutate(Flooded = Flooded_5_Frequency,
-                                               Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
+birth_records_flooded_6_frequency <- matched_mothers_6_frequency %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_6_Frequency,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
-birth_records_flooded_6_frequency <- (birth_records_flooded %>%
-                                        right_join(matched_mothers_6_frequency %>%
-                                                     dplyr::select(caseid, weights, subclass)) %>%
-                                        mutate(Flooded = Flooded_6_Frequency,
-                                               Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
 
-birth_records_flooded_7_frequency <- (birth_records_flooded %>%
-                                        right_join(matched_mothers_7_frequency %>%
-                                                     dplyr::select(caseid, weights, subclass)) %>%
-                                        mutate(Flooded = Flooded_7_Frequency,
-                                               Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
+birth_records_flooded_7_frequency <- matched_mothers_7_frequency %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flooded_7_Frequency,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
 
-# PNAS revision 2
-birth_records_flooded_year <- (birth_records_flooded %>%
-                                        right_join(matched_mothers_flood_year %>%
-                                                     dplyr::select(caseid, weights, subclass)) %>%
-                                        mutate(Flooded = Flood_Year,
-                                               Infant_Death = !(Age_At_Death_Months > 1 | is.na(Age_At_Death_Months)))
-)
+
+# Time-varying exposure for 2003 - 2017
+birth_records_flooded_year <- matched_mothers_flood_year %>%
+  dplyr::select(DHSYEAR, caseid, weights, subclass) %>%
+  mutate(DHSYEAR = as.numeric(paste(DHSYEAR))) %>%
+  left_join(birth_records_flooded, by = c("DHSYEAR","caseid")) %>%
+  mutate(Flooded = Flood_Year,
+         Infant_Death = !(Age_At_Death_Months > 11 | is.na(Age_At_Death_Months))
+  )
+
+
+#-------------------------------------------------------------------------------
+# print number of births and mothers included in the main analysis and in
+# the time-varying analysis
+
+#------------------------------------
+# main analysis
+#------------------------------------
+# number of mothers
+cat("\n The number of unique mothers in the matched dataset is:",paste(nrow(matched_mothers_0_quartile)))
+
+# number of births
+cat("\n The number of unique births in the matched dataset is:",paste(nrow(birth_records_flooded_0_quartile)))
+
+# number of deaths
+cat("\n The number of deaths in the matched dataset is:",paste(sum(birth_records_flooded_0_quartile$Infant_Death)))
+
+
+#------------------------------------
+# time-varying exposure analysis
+#------------------------------------
+# number of mothers
+cat("\n The number of unique mothers in the matched dataset is:",paste(nrow(matched_mothers_flood_year)))
+
+# number of births
+cat("\n The number of unique births in the matched dataset is:",paste(nrow(birth_records_flooded_year)))
+
+# number of deaths
+cat("\n The number of deaths in the matched dataset is:",paste(sum(birth_records_flooded_year$Infant_Death)))
+
 
 #-------------------------------------------------------------------------------
 
