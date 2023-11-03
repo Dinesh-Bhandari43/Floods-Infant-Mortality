e4307a7cf9c135386c4c893398696d97ac6894c8
Ben Arnold <bfarnold@gmail.com>
2023-11-03 15:07:57 -0700

    Minor tweaks to balance fig aesthetics

--- child-mortality-dhs/R/2-analysis/5-matching-covariates-balance.R
+++ child-mortality-dhs/R/2-analysis/5-matching-covariates-balance.R
@@ -11,14 +11,17 @@
 library(here)
 source(here("child-mortality-dhs/R", "0-config.R"))
 
+#-------------------------------------------------------------------------------
+# Love plot for balance in the primary analysis
+
 # Load matched output
 m.out0 <- readRDS(file = here("child-mortality-dhs/output/matching", "matching_0_quartile"))
 
 # Rename covariates
 v <- data.frame(old = c("distance",
-                        "Region_BARISAL", "Region_CHITTAGONG", "Region_DHAKA", "Region_KHULNA", "Region_RAJSHAHI", "Region_RANGPUR", "Region_SYLHET",
+                        "factor(Region)_BARISAL", "factor(Region)_CHITTAGONG", "factor(Region)_DHAKA", "factor(Region)_KHULNA", "factor(Region)_RAJSHAHI", "factor(Region)_RANGPUR", "factor(Region)_SYLHET",
                         "DHSYEAR_2000", "DHSYEAR_2004", "DHSYEAR_2007", "DHSYEAR_2011", "DHSYEAR_2014", "DHSYEAR_2018",
-                        "URBAN_RURA_U",
+                        "factor(URBAN_RURA)_U",
                         "Birth_Date_Mother_Month_CMC",
                         "Highest_Level_Education_higher", "Highest_Level_Education_no education", "Highest_Level_Education_primary", "Highest_Level_Education_secondary",
                         "Wealth_Index_middle", "Wealth_Index_poorer", "Wealth_Index_poorest", "Wealth_Index_richer", "Wealth_Index_richest",
@@ -42,7 +45,7 @@ v <- data.frame(old = c("distance",
                         "Unimproved source of drinking water",
                         "Unimproved sanitation",
                         "Rudimentray floor materials", "Rudimentray wall materials", "Rudimentray roof materials",
-                        "Sanitation facilities shared with other households", "Sanitation facilities not shared with other households",
+                        "Sanitation facilities shared", "Sanitation facilities not shared",
                         "Land surface temperature (2015)", "Rainfall (2015)", "Population Density (2015)"
                         ))
 
@@ -60,10 +63,14 @@ love_plot <- love.plot(m.out0,
           sample.names = c("Unmatched", "Matched"),
           position = "top",
           shapes = c("circle", "circle"),
-          colors = c("#E69F00", "black"))
+          colors = c(cbpal[2], "black"))
 
 
 ### Save
 pdf(here("child-mortality-dhs/output/figures", "love-plot.pdf"))
 love_plot
 dev.off()
+
+
+
+
