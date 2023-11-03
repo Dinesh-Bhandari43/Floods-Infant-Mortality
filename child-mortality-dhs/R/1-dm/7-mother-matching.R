62989134b945ef8a8c4cab74270b3d845af55e6e
Ben Arnold <bfarnold@gmail.com>
2023-11-03 14:59:39 -0700

    Update matching to remove environmental covariates, suggested during peer review

--- child-mortality-dhs/R/1-dm/7-mother-matching.R
+++ child-mortality-dhs/R/1-dm/7-mother-matching.R
@@ -25,8 +25,9 @@ m.out0 <- matchit(factor(Flooded_0_quartile) ~ factor(Region) + DHSYEAR + factor
                     Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                     Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                     Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                    Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                    # remove georeferenced climate variables in response to peer review
+                    # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                   data = mother_records,
                   method = "nearest",
                   exact = "DHSYEAR",
@@ -36,14 +37,16 @@ m.out0 <- matchit(factor(Flooded_0_quartile) ~ factor(Region) + DHSYEAR + factor
                   )
 
 m.data0 <- match.data(m.out0)
+summary(m.out0)
 
 # 1 quartile
 m.out1 <- matchit(factor(Flooded_1_quartile) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                     Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                     Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                     Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                    Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                    # remove georeferenced climate variables in response to peer review
+                    # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                   data = mother_records %>% filter(!(Flooded_0_quartile & !Flooded_1_quartile)),
                   method = "nearest",
                   exact = "DHSYEAR",
@@ -51,14 +54,16 @@ m.out1 <- matchit(factor(Flooded_1_quartile) ~ factor(Region) + DHSYEAR + factor
                   link = "logit")
 
 m.data1 <- match.data(m.out1)
+summary(m.out1)
 
 # 2 quartile
 m.out2 <- matchit(factor(Flooded_2_quartile) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                     Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                     Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                     Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                    Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                    # remove georeferenced climate variables in response to peer review
+                    # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                   data = mother_records %>% filter(!(Flooded_0_quartile & !Flooded_2_quartile)),
                   method = "nearest",
                   exact = "DHSYEAR",
@@ -66,14 +71,16 @@ m.out2 <- matchit(factor(Flooded_2_quartile) ~ factor(Region) + DHSYEAR + factor
                   link = "logit")
 
 m.data2 <- match.data(m.out2)
+summary(m.out2)
 
 # 3 quartile
 m.out3 <- matchit(factor(Flooded_3_quartile) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                     Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                     Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                     Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                    Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                    # remove georeferenced climate variables in response to peer review
+                    # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                   data = mother_records %>% filter(!(Flooded_0_quartile & !Flooded_3_quartile)),
                   method = "nearest",
                   exact = "DHSYEAR",
@@ -81,6 +88,7 @@ m.out3 <- matchit(factor(Flooded_3_quartile) ~ factor(Region) + DHSYEAR + factor
                   link = "logit")
 
 m.data3 <- match.data(m.out3)
+summary(m.out3)
 
 ## Frequency
 # 1
@@ -88,8 +96,9 @@ m.out_1freq <- matchit(factor(Flooded_1_Frequency) ~ factor(Region) + DHSYEAR + 
                     Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                     Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                     Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                    Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                    # remove georeferenced climate variables in response to peer review
+                    # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                   data = mother_records,
                   method = "nearest",
                   exact = "DHSYEAR",
@@ -97,14 +106,16 @@ m.out_1freq <- matchit(factor(Flooded_1_Frequency) ~ factor(Region) + DHSYEAR + 
                   link = "logit")
 
 m.data_1freq <- match.data(m.out_1freq)
+summary(m.out_1freq)
 
 # 2
 m.out_2freq <- matchit(factor(Flooded_2_Frequency) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                          Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                          Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                          Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                         Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                        # remove georeferenced climate variables in response to peer review
+                         # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                        data = mother_records %>% filter(!(Flooded_1_Frequency & !Flooded_2_Frequency)),
                        method = "nearest",
                        exact = "DHSYEAR",
@@ -112,14 +123,16 @@ m.out_2freq <- matchit(factor(Flooded_2_Frequency) ~ factor(Region) + DHSYEAR + 
                        link = "logit")
 
 m.data_2freq <- match.data(m.out_2freq)
+summary(m.out_2freq)
 
 # 3
 m.out_3freq <- matchit(factor(Flooded_3_Frequency) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                          Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                          Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                          Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                         Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                         # remove georeferenced climate variables in response to peer review
+                         # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                        data = mother_records %>% filter(!(Flooded_1_Frequency & !Flooded_3_Frequency)),
                        method = "nearest",
                        exact = "DHSYEAR",
@@ -127,14 +140,16 @@ m.out_3freq <- matchit(factor(Flooded_3_Frequency) ~ factor(Region) + DHSYEAR + 
                        link = "logit")
 
 m.data_3freq <- match.data(m.out_3freq)
+summary(m.out_3freq)
 
 # 4
 m.out_4freq <- matchit(factor(Flooded_4_Frequency) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                          Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                          Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                          Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                         Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                         # remove georeferenced climate variables in response to peer review
+                         # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                        data = mother_records %>% filter(!(Flooded_1_Frequency & !Flooded_4_Frequency)),
                        method = "nearest",
                        exact = "DHSYEAR",
@@ -142,14 +157,16 @@ m.out_4freq <- matchit(factor(Flooded_4_Frequency) ~ factor(Region) + DHSYEAR + 
                        link = "logit")
 
 m.data_4freq <- match.data(m.out_4freq)
+summary(m.out_4freq)
 
 # 5
 m.out_5freq <- matchit(factor(Flooded_5_Frequency) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                          Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                          Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                          Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                         Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                         # remove georeferenced climate variables in response to peer review
+                         # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                        data = mother_records %>% filter(!(Flooded_1_Frequency & !Flooded_5_Frequency)),
                        method = "nearest",
                        exact = "DHSYEAR",
@@ -157,14 +174,16 @@ m.out_5freq <- matchit(factor(Flooded_5_Frequency) ~ factor(Region) + DHSYEAR + 
                        link = "logit")
 
 m.data_5freq <- match.data(m.out_5freq)
+summary(m.out_5freq)
 
 # 6
 m.out_6freq <- matchit(factor(Flooded_6_Frequency) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                          Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                          Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                          Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                         Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                         # remove georeferenced climate variables in response to peer review
+                         # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                        data = mother_records %>% filter(!(Flooded_1_Frequency & !Flooded_6_Frequency)),
                        method = "nearest",
                        exact = "DHSYEAR",
@@ -172,14 +191,16 @@ m.out_6freq <- matchit(factor(Flooded_6_Frequency) ~ factor(Region) + DHSYEAR + 
                        link = "logit")
 
 m.data_6freq <- match.data(m.out_6freq)
+summary(m.out_6freq)
 
 # 2
 m.out_7freq <- matchit(factor(Flooded_7_Frequency) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                          Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                          Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                          Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                         Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                         Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                         # remove georeferenced climate variables in response to peer review
+                         # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                        data = mother_records %>% filter(!(Flooded_1_Frequency & !Flooded_7_Frequency)),
                        method = "nearest",
                        exact = "DHSYEAR",
@@ -187,14 +208,16 @@ m.out_7freq <- matchit(factor(Flooded_7_Frequency) ~ factor(Region) + DHSYEAR + 
                        link = "logit")
 
 m.data_7freq <- match.data(m.out_7freq)
+summary(m.out_7freq)
 
 #PNAS revision 2 Flood_Year
 m.out <- matchit(factor(Flood_Year) ~ factor(Region) + DHSYEAR + factor(URBAN_RURA) +
                     Birth_Date_Mother_Month_CMC + Highest_Level_Education +
                     Wealth_Index + Total_Children + Age_Mother_First_Birth_Years +
                     Source_Of_Drinking_Water + Type_Of_Toilet_Facility + Main_Floor_Material +
-                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH +
-                    Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
+                    Main_Wall_Material + Main_Roof_Material + Toilets_Facilities_Shared_Other_HH,
+                    # remove georeferenced climate variables in response to peer review
+                    # Land_Surface_Temperature_2015 + Rainfall_2015 + UN_Population_Density_2015,
                   data = mother_records,
                   method = "nearest",
                   exact = "DHSYEAR",
@@ -204,6 +227,7 @@ m.out <- matchit(factor(Flood_Year) ~ factor(Region) + DHSYEAR + factor(URBAN_RU
 )
 
 m.data <- match.data(m.out)
+summary(m.out)
 
 
 #-------------------------------------------------------------------------------
